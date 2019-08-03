bc42c32bc2e0627eaa929bc67adbc1c89cd89464
Miguel Lopes <miguel.lopes@miniclip.com>
2019-08-03 12:34:59 +0100

    Create following moving target script

--- 3D-RPG-Medieval-Phantom/Assets/Scripts/PlayerController.cs
+++ 3D-RPG-Medieval-Phantom/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public LayerMask movementMask;
+    public InteractbleObject focus;
     Camera cam;
     PlayerMotor motor;
     // Start is called before the first frame update
@@ -28,6 +30,7 @@ public class PlayerController : MonoBehaviour
                 //Move out player to what we hit
                 motor.MoveToPoint(hit.point);
                 //Stop focusing any objects
+                RemoveFocus();
             }
         }
         if (Input.GetMouseButtonDown(1))
@@ -38,8 +41,26 @@ public class PlayerController : MonoBehaviour
             if (Physics.Raycast(ray, out hit , 100, movementMask))
             {
                 //Check if we hit an interactabçe
+                InteractbleObject interactable =  hit.collider.GetComponent<InteractbleObject>();
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
                 //If we did, set it to as our focus
+
             }
         }
+    }
+
+    private void RemoveFocus()
+    {
+        focus = null;
+        motor.StopFollowingTarget();
+    }
+
+    private void SetFocus(InteractbleObject newFocus)
+    {
+        focus = newFocus;
+        motor.FollowTarget(newFocus);
     }
 }
--- 3D-RPG-Medieval-Phantom/Assets/Scripts/PlayerMotor.cs
+++ 3D-RPG-Medieval-Phantom/Assets/Scripts/PlayerMotor.cs
@@ -6,14 +6,31 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class PlayerMotor : MonoBehaviour
 {
+    Transform target;
     NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
         agent  = GetComponent<NavMeshAgent>();
     }
+    private void Update() {
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
 
     public void MoveToPoint(Vector3 point){
         agent.SetDestination(point);
+    }
+
+    public void FollowTarget(InteractbleObject newTarget){
+        agent.stoppingDistance = newTarget.radius * .8f;
+        target = newTarget.transform;
+    }
+
+    public void StopFollowingTarget(){
+        agent.stoppingDistance = 0f;
+        target = null;
     }
 }
